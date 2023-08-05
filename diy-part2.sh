97d282c38b8c7f85433a0ce2a89915db17fd1038
z991014 <93765018+z991014@users.noreply.github.com>
2023-08-05 13:52:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,9 +15,3 @@ sed -i 's/192.168.1.1/192.168.31.2/g' package/base-files/files/bin/config_genera
 sed -i "s/OpenWrt /Jun build $(TZ=UTC-8 date "+%Y.%m.%d") @ X86 /g" package/lean/default-settings/files/zzz-default-settings
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='Openwrt'' package/lean/default-settings/files/zzz-default-settings
 curl -fsSL https://raw.githubusercontent.com/craytona170/openwrttest/main/banner > package/base-files/files/etc/banner
-sed -i "/exit 0/i\sed -i '/coremark/d' /etc/crontabs/root" "${FIN_PATH}"
-#取消原主题luci-theme-bootstrap为默认主题
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-rm -rf feeds/luci/themes/luci-theme-bootstrap/
