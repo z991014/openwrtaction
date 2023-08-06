4715ce3110ddf0ee0eaac8cb68cf8c7f12f60677
z991014 <93765018+z991014@users.noreply.github.com>
2023-08-06 18:03:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.31.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.4/g' package/base-files/files/bin/config_generate
 sed -i "s/OpenWrt /Jun build $(TZ=UTC-8 date "+%Y.%m.%d") @ X86 /g" package/lean/default-settings/files/zzz-default-settings
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='Openwrt'' package/lean/default-settings/files/zzz-default-settings
 curl -fsSL https://raw.githubusercontent.com/craytona170/openwrttest/main/banner > package/base-files/files/etc/banner
